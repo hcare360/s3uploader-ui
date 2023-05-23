3c3b3de01c65e4ef45558445a350ddadb1abaabc
divyavanmahajan <divyavanmahajan@users.noreply.github.com>
2023-05-23 06:44:49 +0200

    Switch to cloudscape design

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "brooksstorage11a5c3ba9": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "S3Trigger54579c95": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "filestore": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
-import "@awsui/global-styles/index.css";
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
+import "@cloudscape-design/global-styles/index.css";
+import App from "./App";
 
 ReactDOM.render(
-    <HashRouter>
-        <App/>
-    </HashRouter>,
-    document.getElementById('root')
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById("root")
 );
